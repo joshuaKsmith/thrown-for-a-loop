6060261f79b4a7856c0959d716e5b9a10d63b01d
Joshua Smith <mtgmirage@protonmail.com>
2024-11-18 14:17:47 -0600

    hello world more or less

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string greeting = @"Welcome to Thrown for a Loop
+Your one-stop shop for used sporting equipment";
+
+Console.WriteLine(greeting);
+
+Console.WriteLine("Please choose an option: ");
+
+string response = Console.ReadLine().Trim();
+
+while (string.IsNullOrEmpty(response))
+{
+    Console.WriteLine("You didn't choose anything, try again!");
+    response = Console.ReadLine().Trim();
+}
+Console.WriteLine(@$"You chose: {response}");
+
+
