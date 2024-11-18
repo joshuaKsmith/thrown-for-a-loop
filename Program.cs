99ccb45b17c5526753ac174ed0a13ade4a88004f
Joshua Smith <mtgmirage@protonmail.com>
2024-11-18 14:39:01 -0600

    implemented List

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
-﻿string greeting = @"Welcome to Thrown for a Loop
+﻿
+List<string> products = new List<string>()
+{
+    "Football",
+    "Hockey Stick",
+    "Boomerang",
+    "Frisbee",
+    "Golf Putter"
+};
+
+string greeting = @"Welcome to Thrown for a Loop
 Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
-
-Console.WriteLine("Please choose an option: ");
-
-string response = Console.ReadLine().Trim();
-
-while (string.IsNullOrEmpty(response))
+Console.WriteLine("Products:");
+for (int i=0; i < products.Count; i++)
 {
-    Console.WriteLine("You didn't choose anything, try again!");
-    response = Console.ReadLine().Trim();
+    Console.WriteLine($"{i+1}. {products[i]}");
 }
-Console.WriteLine(@$"You chose: {response}");
+Console.WriteLine("Please enter a product number: ");
+
+int response = int.Parse(Console.ReadLine().Trim());
+
+while (response > products.Count || response < 1)
+{
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
+}
+Console.WriteLine(@$"You chose: {products[response - 1]}");
 
 
