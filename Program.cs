ff11cb9634f32f6e94f8ae850af6d5e86248f228
Joshua Smith <mtgmirage@protonmail.com>
2024-11-20 14:31:28 -0600

    whitespace

--- Program.cs
+++ Program.cs
@@ -59,11 +59,11 @@ List<Product> products = new List<Product>()
     }
 };
 
-string greeting = @"Welcome to Thrown for a Loop
-Your one-stop shop for used sporting equipment";
+Console.WriteLine(@"Welcome to Thrown for a Loop
+Your one-stop shop for used sporting equipment");
 
-Console.WriteLine(greeting);
 Console.WriteLine("Products:");
+
 for (int i=0; i < products.Count; i++)
 {
     Console.WriteLine($"{i+1}. {products[i].Name}");
@@ -77,11 +77,11 @@ while (response > products.Count || response < 1)
     Console.WriteLine($"Choose a number between 1 and {products.Count}!");
     response = int.Parse(Console.ReadLine().Trim());
 }
+
 Product chosenProduct = products[response - 1];
-
 DateTime now = DateTime.Now;
-
 TimeSpan timeInStock = now - chosenProduct.StockDate;
+
 Console.WriteLine(@$"You chose: 
 {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
 It is {now.Year - chosenProduct.ManufactureYear} years old. 
